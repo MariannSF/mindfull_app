864bc777fba7491aa0788ad5926db2f4a557fd0e
Mariann <mariann.freund@gmail.com>
2024-11-01 16:05:19 -0400

    Welcome Screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -6,9 +6,7 @@ class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome Screen'),
-      ),
+      
       body: Stack(
         children: [
           Positioned.fill(
@@ -16,11 +14,23 @@ class IntroScreen extends StatelessWidget {
             'assets/Sea.jpg',
             fit: BoxFit.cover,
           )),
-          Center(
+          const Align(
+            alignment: Alignment(0, -0.5),
               child: Text(
             'Welcome',
-            style: TextStyle(fontSize: 24),
-          ))
+            style: TextStyle(
+              color: Colors.white,
+              shadows: [Shadow(
+                blurRadius: 10, 
+                color: Colors.black,
+                offset: Offset(5, 5))],
+              fontSize: 24),
+          )),
+          Align(alignment: const Alignment(0, 0.5),
+            child: ElevatedButton(
+              onPressed: () {},
+               child: const Text('Start')),
+          )
         ],
       ),
     );
