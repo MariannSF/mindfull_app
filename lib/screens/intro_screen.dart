61e37a301b8cca2ad57ac1dc2062338de5ecb027
Mariann <mariann.freund@gmail.com>
2024-11-08 17:23:24 -0500

    added setting_screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:mindfull_app_2/data/sp_helper.dart';
+import 'package:mindfull_app_2/screens/quote_screen.dart';
 import 'package:mindfull_app_2/screens/setting_screen.dart';
 
-class IntroScreen extends StatelessWidget {
+class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
 
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+
+  String name ='';
+  String image = '';
+
+  @override
+  void initState() {
+    
+    super.initState();
+    final helper = SpHelper();
+    helper.getSettings().then((settings) {
+      name = settings['name'] ??  '';
+      image = settings['image'] ?? 'Lake';
+      setState(() {
+        
+      });
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,14 +37,14 @@ class IntroScreen extends StatelessWidget {
         children: [
           Positioned.fill(
               child: Image.asset(
-            'assets/Sea.jpg',
+            'assets/$image.jpg',
             fit: BoxFit.cover,
           )),
-          const Align(
-            alignment: Alignment(0, -0.5),
+          Align(
+            alignment: const Alignment(0, -0.5),
               child: Text(
-            'Welcome',
-            style: TextStyle(
+            "Welcome $name",
+            style:  const TextStyle(
               color: Colors.white,
               shadows: [Shadow(
                 blurRadius: 10, 
@@ -30,9 +55,9 @@ class IntroScreen extends StatelessWidget {
           Align(alignment: const Alignment(0, 0.5),
             child: ElevatedButton(
               onPressed: () {
-                Navigator.of(context).push(
+                Navigator.of(context).pushReplacement(
                   MaterialPageRoute(
-                    builder: (BuildContext context) => const SettingsScreen())
+                    builder: (BuildContext context) => const QuoteScreen())
                 );
               },
                child: const Text('Start')),
--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mindfull_app_2/data/sp_helper.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -9,11 +10,83 @@ class SettingsScreen extends StatefulWidget {
 
 //we create the UI in the state which is below
 class _SettingsScreenState extends State<SettingsScreen> {
+  
+  final TextEditingController txtName = TextEditingController();
+  final List<String> _images = ['Lake', 'Mountain', 'Sea', 'Country'];
+  String _selectedImage = 'Lake';
+   final SpHelper helper = SpHelper();
+
+  @override
+  void initState() {
+   
+    super.initState();
+
+    getSettings();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings'),),
-      body: const Placeholder() ,
+      body:  Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [TextField(
+            controller: txtName,
+            decoration:  const InputDecoration(hintText: 'Enter your name' ),
+          ),
+          DropdownButton <String> (
+            value: _selectedImage,
+            items: _images.map((String value) {
+              return DropdownMenuItem(
+                value: value,
+                
+                child: Text(value));
+
+            }).toList() ,
+            onChanged: (newvalue) {
+              setState(() {
+                _selectedImage = newvalue ?? 'Lake';
+              });
+            })
+          ],
+          
+        
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+         saveSettings().then((value){
+          String message = value? 'The settings have been saved'
+          :'Error: the settings were not saved';
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(message),
+          duration: const Duration(seconds: 3),)
+          );
+         });
+        },
+      child: const Icon(Icons.save),),
     );
+  }
+
+  Future<bool> saveSettings()async{
+    return await helper.setSettings(txtName.text, _selectedImage);
+  }
+  
+Future<void> getSettings() async {
+  Map<String,String> settings = await SpHelper().getSettings();
+  
+  setState(() {
+    _selectedImage = settings['image'] ?? 'Lake';
+    txtName.text = settings['name'] ?? '';
+  });
+}
+
+@override
+  void dispose() {
+    txtName.dispose();
+    super.dispose();
   }
 }
